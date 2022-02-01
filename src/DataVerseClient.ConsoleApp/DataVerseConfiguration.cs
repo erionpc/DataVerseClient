b0e3abc8f3327248ed219fdd8498407a3ec678cf
Erion Pici <erion.pici@thirdspace.net>
2022-02-01 11:58:47 +0000

    created sample solution

---
+++ src/DataVerseClient.ConsoleApp/DataVerseConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVerseApiSample.ConsoleApp
+{
+    public class DataVerseConfiguration
+    {
+        public string? Url { get; set; }
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+        public bool UseUniqueInstance { get; set; }
+
+        public bool IsValid() =>
+            !string.IsNullOrWhiteSpace(Url) && 
+            !string.IsNullOrWhiteSpace(ClientId) && 
+            !string.IsNullOrWhiteSpace(ClientSecret);
+    }
+}
---
+++ src/DataVerseClient.ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using DataVerseApiSample.ConsoleApp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk.Query;
+
+try
+{
+    IConfiguration config =
+        new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+
+    var dataVerseConfig = new DataVerseConfiguration();
+    config.GetSection("DataVerseConfiguration").Bind(dataVerseConfig);
+
+    if (!dataVerseConfig.IsValid())
+        throw new Exception("Invalid configuration");
+
+    var serviceClient = new ServiceClient(new Uri(dataVerseConfig.Url ?? ""), dataVerseConfig.ClientId, dataVerseConfig.ClientSecret, true);
+
+    Console.WriteLine($"Connected to the DataVerse web-API: {serviceClient.ConnectedOrgFriendlyName}");
+
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine($"{System.Environment.NewLine}Write name of entity to query:");
+            string entityName = Console.ReadLine() ?? "";
+            Console.WriteLine($"Querying entity: {entityName}{System.Environment.NewLine}");
+
+            var entitiesFound = await serviceClient.RetrieveMultipleAsync(new QueryExpression()
+            {
+                EntityName = entityName,
+                TopCount = 10
+            });
+
+            Console.WriteLine($"Found: {entitiesFound.TotalRecordCount}");
+
+            Console.WriteLine(string.Join(System.Environment.NewLine,
+                entitiesFound.Entities.ToString()));
+            
+            Console.WriteLine();
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
